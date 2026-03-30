e3251c5fdaa57620e4423359d7850343e8dd6953
himanshupatel2007 <himanshupatel.942007@gmail.com>
2026-03-30 19:11:38 +0530

    learned how to put files in S3 via Signed URLs

--- S3/index.js
+++ S3/index.js
@@ -1,11 +1,11 @@
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3") 
+const { S3Client, GetObjectCommand ,PutObjectCommand} = require("@aws-sdk/client-s3") 
 const {getSignedUrl} = require("@aws-sdk/s3-request-presigner") 
 
 const s3Client = new S3Client({
     region: "ap-south-1",
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: "",
+        secretAccessKey: ""
     }
 
 })
@@ -18,8 +18,19 @@ async function getObjectURl(key) {
     const URl = await getSignedUrl(s3Client,command,{expiresIn:30});
     return URl;
 }
+async function putObject(filename,contentType){
+    const command = new PutObjectCommand({
+        Bucket: "learningbucket-one",
+        Key:`/uploads/files/${filename}`,
+        ContentType:contentType,
+    });
+    const url = await getSignedUrl(s3Client,command);
+    return url;
+
+}
 async function URL() {
     
-    console.log("URL for Object : BlackWindowWallpaper.jpg is ",await getObjectURl("BlackWindowWallpaper.jpg"))
+   console.log("URL for Object : BlackWindowWallpaper.jpg is ",await getObjectURl("/uploads/files/image - 1774877458217.png"))
+  // console.log("URL for Uploading Files :", await putObject(`image - ${Date.now()}.png`))
 }
 URL();
